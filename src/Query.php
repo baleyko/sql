a0cedcd9a2805171de3a77506aff5717d526abd4
Valery Baleyko <baleyko@gmail.com>
2014-01-22 22:55:47 +0200

    Almost empty skeleton.

---
+++ src/Query.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is part of the SQL package.
+ *
+ * (c) Valery Baleyko <baleyko@gmail.com>
+ */
+
+namespace SQL;
+
+/**
+ * This class provides some tools to build SQL queries.
+ *
+ * @author Valery Baleyko <baleyko@gmail.com>
+ */
+class Query
+{
+}
